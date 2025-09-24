04ba2065dfdc3efc731280fe517f546c2923da2c
vfong3 <vfong@berkeley.edu>
2025-09-24 12:10:31 -0400

    Initial commit of ARGOSTrackingTool script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Vicky Fong (vicky.fong@duke.edu)
+# Date:   Fall 2025
+#--------------------------------------------------------------
