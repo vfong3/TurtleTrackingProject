6a1709296199a02cefa0af2c4ecfe8134fea7366
vfong3 <vfong@berkeley.edu>
2025-10-01 12:20:10 -0400

    Revert "For loop to process all sara.txt data"
    This reverts commit 62f1ad70ecb225b9a185ec3e1fca2635fe69603d.

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -16,13 +16,15 @@ file_name = './data/raw/Sara.txt'
 file_object = open(file_name,'r')
 
 #Read contents of file into a list
-lineString = file_object.readline()
+line_list = file_object.readlines()
 
-#Read in all data
-while lineString:
+#Close the file
+file_object.close()
+
+#Pretend we read one line of data from the file
+for lineString in line_list:
     #Check if line is data line
     if lineString[0] in ("#","u"):
-        lineString = file_object.readline()
         continue
 
     #Split the string into a list of data items
@@ -37,9 +39,3 @@ while lineString:
 
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
-
-    # Move to the next line
-    lineString = file_object.readline()
-
-# Close the file
-file_object.close()
