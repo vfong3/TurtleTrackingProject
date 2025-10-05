44a7bc44b1e77c59d54f6adff43e97e5fe726daf
vfong3 <vfong@berkeley.edu>
2025-10-05 13:58:30 -0400

    Filter records going into dictionary

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -21,6 +21,10 @@ line_list = file_object.readlines()
 #Close the file
 file_object.close()
 
+#Initialize dictionaries
+date_dict = {}
+location_dict = {}
+
 #Pretend we read one line of data from the file
 for lineString in line_list:
     #Check if line is data line
@@ -37,5 +41,11 @@ for lineString in line_list:
     obs_lat = lineData[6]
     obs_lon = lineData[7]
 
+    #Determine if location class criteria is met
+    if obs_lc in ("1","2","3"):
+        #Add items to dictionaries
+        date_dict[record_id] = obs_date
+        location_dict[record_id] = (obs_lat, obs_lon)
+
     #Print the location of sara
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
