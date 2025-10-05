db568d9c1b3bb33b09c7ccd7fa4ccc4709f37730
vfong3 <vfong@berkeley.edu>
2025-10-05 14:36:44 -0400

    Add "no records found" statement

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -58,6 +58,10 @@ for key, value in date_dict.items():
     if value == user_date: 
         keys.append(key)
 
+#Report if no records were found
+if len(keys) == 0:
+    print(f"No records were found on {user_date}.")
+
 #Loop through keys and report locations
 for key in keys: 
     location = location_dict[key]
